ae11ee6fbd79e0ebb8d9f7ec6bce00494b86ada9
Felix Hultman <karlfelixhultman@gmail.com>
2024-03-15 10:22:41 +0100

    first tests and functionality

--- wordFeedback.js
+++ wordFeedback.js
@@ -1,3 +1,28 @@
-export default function wordFeedback(str) {
-  return str;
+export default function wordFeedback(guessWord, correctWord) {
+  const feedback = [];
+
+  for (let i = 0; i < guessWord.length; i++) {
+    const guessedLetter = guessWord[i];
+    const correctLetter = correctWord[i];
+
+    if (guessedLetter === correctLetter) {
+      feedback.push({
+        letter: guessedLetter,
+        result: 'You got it 100% right!',
+      });
+    } else if (correctWord.includes(guessedLetter)) {
+      feedback.push({
+        letter: guessedLetter,
+        result: 'Right letter, wrong place!',
+      });
+    } else {
+      feedback.push({
+        letter: guessedLetter,
+        result: 'Not in the ballpark, bro',
+      });
+    }
+  }
+  return feedback;
 }
+
+console.log(wordFeedback('Football', 'hand'));
--- wordFeedback.spec.js
+++ wordFeedback.spec.js
@@ -1,3 +1,22 @@
 import { describe, expect, it } from '@jest/globals';
 
 import wordFeedback from './wordFeedback.js';
+
+describe('wordFeedback()', () => {
+  // testar att en tom sträng ger en tom array
+  it('Returns an empty array if input is an empty string', () => {
+    const output = wordFeedback('');
+    expect(output).toEqual([]);
+  });
+  // Testar att korrekt feedback ges om inga bokstäver stämmer övers med rätt ord
+  it('If no letters matches the correct word, all letters give incorrect', () => {
+    const output = wordFeedback('glow', 'hand');
+    const expectedOutput = [
+      { letter: 'g', result: 'Not in the ballpark, bro' },
+      { letter: 'l', result: 'Not in the ballpark, bro' },
+      { letter: 'o', result: 'Not in the ballpark, bro' },
+      { letter: 'w', result: 'Not in the ballpark, bro' },
+    ];
+    expect(output).toEqual(expectedOutput);
+  });
+});
