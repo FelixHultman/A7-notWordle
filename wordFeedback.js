055108c45bb2f9ab908f32ee5f06d63b05121937
Felix Hultman <karlfelixhultman@gmail.com>
2024-03-17 20:32:39 +0100

    more test and logic

--- wordFeedback.js
+++ wordFeedback.js
@@ -1,16 +1,26 @@
 export default function wordFeedback(guessWord, correctWord) {
+  if (!guessWord || !correctWord) {
+    return [];
+  }
+
+  const guessArray = guessWord.split('');
+  const correctArray = correctWord.split('');
   const feedback = [];
 
-  for (let i = 0; i < guessWord.length; i++) {
-    const guessedLetter = guessWord[i];
-    const correctLetter = correctWord[i];
-
+  for (let i = 0; i < guessArray.length; i++) {
+    const guessedLetter = guessArray[i];
+    const correctLetter = correctArray[i];
+    console.log('guessedLetter:', guessedLetter);
+    console.log('correctLetter:', correctLetter);
     if (guessedLetter === correctLetter) {
       feedback.push({
         letter: guessedLetter,
         result: 'You got it 100% right!',
       });
-    } else if (correctWord.includes(guessedLetter)) {
+    } else if (
+      correctArray.includes(guessedLetter) &&
+      correctArray.indexOf(guessedLetter) !== i
+    ) {
       feedback.push({
         letter: guessedLetter,
         result: 'Right letter, wrong place!',
@@ -21,8 +31,11 @@ export default function wordFeedback(guessWord, correctWord) {
         result: 'Not in the ballpark, bro',
       });
     }
+    console.log('correctarray:', correctArray);
+    console.log('guessArray:', guessArray);
   }
+
   return feedback;
 }
 
-console.log(wordFeedback('Football', 'hand'));
+console.log(wordFeedback('hallå', 'cykla'));
--- wordFeedback.spec.js
+++ wordFeedback.spec.js
@@ -8,14 +8,39 @@ describe('wordFeedback()', () => {
     const output = wordFeedback('');
     expect(output).toEqual([]);
   });
-  // Testar att korrekt feedback ges om inga bokstäver stämmer övers med rätt ord
+  // Testar att korrekt feedback ges om inga bokstäver stämmer överens med rätt ord
   it('If no letters matches the correct word, all letters give incorrect', () => {
-    const output = wordFeedback('glow', 'hand');
+    const output = wordFeedback('glows', 'handy');
     const expectedOutput = [
       { letter: 'g', result: 'Not in the ballpark, bro' },
       { letter: 'l', result: 'Not in the ballpark, bro' },
       { letter: 'o', result: 'Not in the ballpark, bro' },
       { letter: 'w', result: 'Not in the ballpark, bro' },
+      { letter: 's', result: 'Not in the ballpark, bro' },
+    ];
+    expect(output).toEqual(expectedOutput);
+  });
+  // Testar att korrekt feedback ges om alla bokstäver stämmer överens med rätt ord
+  it('If all letters matches the correct word, all letters give correct', () => {
+    const output = wordFeedback('hands', 'hands');
+    const expectedOutput = [
+      { letter: 'h', result: 'You got it 100% right!' },
+      { letter: 'a', result: 'You got it 100% right!' },
+      { letter: 'n', result: 'You got it 100% right!' },
+      { letter: 'd', result: 'You got it 100% right!' },
+      { letter: 's', result: 'You got it 100% right!' },
+    ];
+    expect(output).toEqual(expectedOutput);
+  });
+  // Om en bokstav förekommer två gånger i gissning och svar ska rätt feedback ges för båda
+  it('if one letter is used more than once in both guess and answer, it housld return the right feedback for both', () => {
+    const output = wordFeedback('hallå', 'cykla');
+    const expectedOutput = [
+      { letter: 'h', result: 'Not in the ballpark, bro' },
+      { letter: 'a', result: 'Right letter, wrong place!' },
+      { letter: 'l', result: 'Not in the ballpark, bro' },
+      { letter: 'l', result: 'You got it 100% right!' },
+      { letter: 'å', result: 'Not in the ballpark, bro' },
     ];
     expect(output).toEqual(expectedOutput);
   });
