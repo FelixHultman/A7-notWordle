bdd70c86dac42618f20e83ead0cedb5bcbbbcde1
Felix Hultman <karlfelixhultman@gmail.com>
2024-03-18 13:56:42 +0100

    fixed logic for same letter appering more than once in guess but not i answer

--- wordFeedback.js
+++ wordFeedback.js
@@ -6,12 +6,19 @@ export default function wordFeedback(guessWord, correctWord) {
   const guessArray = guessWord.split('');
   const correctArray = correctWord.split('');
   const feedback = [];
+  const correctlyGuessed = new Set();
 
-  for (let i = 0; i < guessArray.length; i++) {
+  for (let i = 0; i < correctArray.length; i++) {
+    const correctLetter = correctArray[i];
+    if (guessArray[i] === correctLetter) {
+      correctlyGuessed.add(correctLetter);
+    }
+  }
+
+  for (let i = 0; i < correctArray.length; i++) {
     const guessedLetter = guessArray[i];
     const correctLetter = correctArray[i];
-    console.log('guessedLetter:', guessedLetter);
-    console.log('correctLetter:', correctLetter);
+
     if (guessedLetter === correctLetter) {
       feedback.push({
         letter: guessedLetter,
@@ -19,7 +26,7 @@ export default function wordFeedback(guessWord, correctWord) {
       });
     } else if (
       correctArray.includes(guessedLetter) &&
-      correctArray.indexOf(guessedLetter) !== i
+      !correctlyGuessed.has(guessedLetter)
     ) {
       feedback.push({
         letter: guessedLetter,
@@ -31,10 +38,7 @@ export default function wordFeedback(guessWord, correctWord) {
         result: 'Not in the ballpark, bro',
       });
     }
-    console.log('correctarray:', correctArray);
-    console.log('guessArray:', guessArray);
   }
-
   return feedback;
 }
 
